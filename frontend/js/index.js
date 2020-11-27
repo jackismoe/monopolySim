cc549e4adf0f69e56d934df11993e6c46fde01f4
jackismoe <jacksonmo@me.com>
2020-11-26 22:03:46 -0500

    Create player objects and setup form

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,8 +5,8 @@ const bottomRowContainer = document.querySelector('.bottomRow')
 const game = document.querySelector('.game')
 const gameBoard = document.querySelector('.gameBoard')
 const setup = document.querySelector('.setup')
-const submitSetup = document.querySelector('#submitSetup')
 const showCard = document.createElement('div')
+const body = document.querySelector('body')
 
 showCard.className = 'showCard'
 
@@ -32,11 +32,65 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     GameSquare.gameSquares[i].createSquare(div)
   }
-  
-  submitSetup.addEventListener('click', (e) => {
-    e.preventDefault()
-    
-    setup.style.visibility = 'hidden'
-    game.style.visibility = 'visible'
-  })
+
+  getSetUpValues()
 })
+
+function getSetUpValues() {
+  let setup = document.createElement('form')
+  setup.className = 'setup'
+  
+  let playerSelect = document.createElement('div')
+  let cashSelect = document.createElement('div')
+  let welcome = document.createElement('h1')
+  welcome.className = 'welcome'
+  welcome.innerText = 'monopolySim'
+  playerSelect.className = 'playerSelect'
+  cashSelect.className = 'cashSelect'
+  playerSelect.innerHTML = `<h2>Player Names</h2>
+                            <label>Player 1</label><br>
+                            <input placeholder='Jack'></input><br>
+                            <label>Player 2</label><br>
+                            <input placeholder='Blue'></input><br>
+                            <label>Player 3</label><br>
+                            <input placeholder='Ruby'></input><br>
+                            <label>Player 4</label><br>
+                            <input placeholder='Smudge'></input>`
+
+  cashSelect.innerHTML = `<div>
+                            <h2>Starting Cash</h2>
+                            <select>
+                              <option value="0"></option>
+                              <option value="1">1500</option>
+                              <option value="2">2500</option>
+                              <option value="3">5000</option>
+                              <option value="4">10,000</option>
+                            </select>
+                          </div>`
+
+  let submit = document.createElement('button')
+  submit.type = 'submit'
+  submit.innerText = 'Start Game!'
+  setup.appendChild(playerSelect)
+  setup.appendChild(cashSelect)
+  setup.appendChild(submit)
+  body.appendChild(welcome)
+  body.appendChild(setup)
+
+  submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    let playerOne = e.target.parentElement[0].value
+    let playerTwo = e.target.parentElement[1].value
+    let playerThree = e.target.parentElement[2].value
+    let playerFour = e.target.parentElement[3].value
+    let startingCash = e.target.parentElement[4].value
+    if (startingCash == 0) {
+      alert('You must select a starting cash value to proceed.')
+    } else {
+      Player.create(playerOne, playerTwo, playerThree, playerFour, startingCash)
+      setup.remove()
+      welcome.remove()
+      game.style.visibility = 'visible'
+    }
+  })
+}
--- frontend/js/player.js
+++ frontend/js/player.js
@@ -1,22 +1,50 @@
 class Player {
-// money
-  // need to be able to subrtract or add to based on value of the square landed on
-// game piece
-  // need to be able to keep track of where a player is on the board
-// properties owned
-  // has a price
-  // has a mortgage
-
-  constructor(name) {
+  static currentPlayers = []
+  constructor(name, cash) {
     this.name = name
-    this.money = 1500
+    this.cash = cash
     this.propertiesOwned = {}
     this.position = 1
 
     if (this.position > 40) {
       this.position = 1
     }
+    Player.currentPlayers.push(this)
   }
 
-  
+  static create(playerOne, playerTwo, playerThree, playerFour, startingCash) {
+    let cash
+    let one
+    let two
+    let three
+    let four
+
+    if (startingCash == 1) {
+      cash = 1500
+    } else if (startingCash == 2) {
+      cash = 2500
+    } else if (startingCash == 3) {
+      cash = 5000
+    } else if (startingCash == 4) {
+      cash = 10000
+    }
+
+    playerOne == '' ? one = 'jack' : one = playerOne
+    playerTwo == '' ? two = 'blue' : two = playerTwo
+    playerThree == '' ?  three = 'ruby' : three = playerThree
+    playerFour == '' ? four = 'smudge' : four = playerFour
+
+    new Player(one, cash)
+    new Player(two, cash)
+    new Player(three, cash)
+    new Player(four, cash)
+
+    Player.buildPlayerCards()
+  }
+
+  static buildPlayerCards() {
+    for (let x of Player.currentPlayers) {
+      console.log(x)
+    }
+  }
 }
