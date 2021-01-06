48de6905513760a04ba296e4abc5b63176f372fb
Jack <jacksonmorgan@hey.com>
2021-01-06 09:02:56 -0500

    change variable names

--- monopoly-frontend/src/components/stateful/GameSquare.jsx
+++ monopoly-frontend/src/components/stateful/GameSquare.jsx
@@ -6,8 +6,8 @@ class GameSquare extends React.Component {
 
   render() {
     const middleNine = this.props.squares.filter(square => square.spaceNumber == 12|| square.spaceNumber == 13|| square.spaceNumber == 14|| square.spaceNumber == 15|| square.spaceNumber == 16|| square.spaceNumber == 17|| square.spaceNumber == 18|| square.spaceNumber == 19|| square.spaceNumber == 20|| square.spaceNumber == 32|| square.spaceNumber == 33|| square.spaceNumber == 34|| square.spaceNumber == 35|| square.spaceNumber == 36|| square.spaceNumber == 37|| square.spaceNumber == 38|| square.spaceNumber == 39|| square.spaceNumber == 40)
-    const bottomRow = this.props.squares.filter(square => square.spaceNumber > 20 && square.spaceNumber < 32)
-    const topRow = this.props.squares.filter(square => square.spaceNumber < 12)
+    const topRow = this.props.squares.filter(square => square.spaceNumber > 20 && square.spaceNumber < 32)
+    const bottomRow = this.props.squares.filter(square => square.spaceNumber < 12)
 
     return(
       <>
